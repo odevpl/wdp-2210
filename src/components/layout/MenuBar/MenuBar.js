d2089ad3b088a81d526f6187e496d25cf1562010
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-10-07 19:09:33 +0200

    Add RWD to main menu

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -1,48 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
+import clsx from 'clsx';
 import ProductSearch from '../../features/ProductSearch/ProductSearch';
 
 import styles from './MenuBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const MenuBar = ({ children }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className='col'>
-          <ProductSearch />
-        </div>
-        <div className={'col-auto ' + styles.menu}>
-          <ul>
-            <li>
-              <a href='#' className={styles.active}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>Furniture</a>
-            </li>
-            <li>
-              <a href='#'>Chair</a>
-            </li>
-            <li>
-              <a href='#'>Table</a>
-            </li>
-            <li>
-              <a href='#'>Sofa</a>
-            </li>
-            <li>
-              <a href='#'>Bedroom</a>
-            </li>
-            <li>
-              <a href='#'>Blog</a>
-            </li>
-          </ul>
-        </div>
+const MenuBar = ({ children }) => {
+  const [addClass, setaddClass] = useState(false);
+
+  const handleClick = e => {
+    e.preventDefault();
+    setaddClass(addClass => !addClass);
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <nav className={styles.navbar + ' row align-items-center'}>
+          <div className={'col-lg-6 ' + styles.searchDiv}>
+            <ProductSearch />
+          </div>
+          <a onClick={handleClick} href='#' className={styles.toggle_button}>
+            <FontAwesomeIcon icon={faBars} />
+          </a>
+          <div
+            className={clsx(
+              'col-lg-6 ' + styles.menu,
+              addClass && styles.active + ' col-12'
+            )}
+          >
+            <ul>
+              <li>
+                <a href='#' className={styles.active}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#'>Furniture</a>
+              </li>
+              <li>
+                <a href='#'>Chair</a>
+              </li>
+              <li>
+                <a href='#'>Table</a>
+              </li>
+              <li>
+                <a href='#'>Sofa</a>
+              </li>
+              <li>
+                <a href='#'>Bedroom</a>
+              </li>
+              <li>
+                <a href='#'>Blog</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 MenuBar.propTypes = {
   children: PropTypes.node,
