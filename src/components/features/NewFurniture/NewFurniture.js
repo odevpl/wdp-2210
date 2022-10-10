90156c6c4c3583c20fa19210dde5d09572c75ee0
Adam Mocarski <adammocarski22@gmail.com>
2022-10-10 15:38:15 +0200

    Add useSelecor, use store to input data,

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './NewFurniture.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
 import Swipeable from '../../common/Swipeable/Swipeable';
