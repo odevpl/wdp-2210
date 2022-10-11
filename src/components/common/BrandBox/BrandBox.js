5758da84a55881201829c0c7d5a148b3bdf55c19
Adam Mocarski <adammocarski22@gmail.com>
2022-10-11 18:10:06 +0200

    Ad fa, images

--- src/components/common/BrandBox/BrandBox.js
+++ src/components/common/BrandBox/BrandBox.js
@@ -1,16 +1,43 @@
-import React from 'react0';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styles from './BrandBox.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArrowAltCircleLeft,
+  faArrowAltCircleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 const BrandBox = () => {
-
-
-  return(
-
-    <div className="container">
-      <div className="brandImageBox">
-        <div className="brandImage">
-            <img src
+  const brands = useSelector(store => store.brands);
+  console.log(brands);
+  return (
+    <div className='container'>
+      <div className={styles.brandImageBox}>
+        <div className={styles.faBox}>
+          <FontAwesomeIcon
+            className={styles.fa}
+            icon={faArrowAltCircleLeft}
+            faAlignCenter
+          />
+        </div>
+        {brands.map(brand => (
+          <div className={styles.brandImage} key={brand.id}>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/Brands/${brand.name}.jpg`}
+              alt='sofa'
+            />
+          </div>
+        ))}
+        <div className={styles.faBox}>
+          <FontAwesomeIcon
+            className={styles.fa}
+            icon={faArrowAltCircleRight}
+            faAlignCenter
+          />
         </div>
       </div>
     </div>
   );
 };
+
+export default BrandBox;
