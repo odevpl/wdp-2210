31141bbea97142bd4e71184d9a61fd8aa511200f
Erykov9 <szczepanekeryk@gmail.com>
2022-10-12 22:59:41 +0200

    done adding stars rating WDP221001-21

--- src/components/common/StarCounter/StarCounter.js
+++ src/components/common/StarCounter/StarCounter.js
@@ -5,34 +5,53 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { editStars } from '../../../redux/productsRedux';
 
 import styles from './StarCounter.module.scss';
 
 const StarCounter = ({ stars, item }) => {
-  const starsHandler = e => {
+  const dispatch = useDispatch();
+
+  const valueHandler = (e, i) => {
     e.preventDefault();
-    console.log(item);
+    dispatch(editStars({ ...item, myStars: i }));
   };
 
-  const valueHandler = e => {
-    e.preventDefault();
-    console.log(e.target.getAttribute('value'));
-    console.log(e.target);
-  };
-
-  return (
-    <div className={styles.stars} onMouseEnter={starsHandler}>
-      {[1, 2, 3, 4, 5].map(i => (
-        <a key={i} href='#' onClick={valueHandler}>
-          {i <= stars ? (
-            <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-          ) : (
-            <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-          )}
-        </a>
-      ))}
-    </div>
-  );
+  if (item.myStars) {
+    return (
+      <div className={styles.stars}>
+        {[1, 2, 3, 4, 5].map(i => (
+          <a
+            key={i}
+            className={styles.active}
+            href='#'
+            onClick={e => valueHandler(e, i)}
+          >
+            {i <= item.myStars ? (
+              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+            )}
+          </a>
+        ))}
+      </div>
+    );
+  } else if (!item.myStars) {
+    return (
+      <div className={styles.stars}>
+        {[1, 2, 3, 4, 5].map(i => (
+          <a key={i} href='#' onClick={e => valueHandler(e, i)}>
+            {i <= stars ? (
+              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+            )}
+          </a>
+        ))}
+      </div>
+    );
+  }
 };
 
 StarCounter.propTypes = {
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,17 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+export const editStars = payload => ({ type: 'EDIT', payload });
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case 'EDIT':
+      return statePart.map(products =>
+        products.id === action.payload.id
+          ? { ...products, ...action.payload }
+          : products
+      );
     default:
       return statePart;
   }
