b5ba0d9e048bcd76105c84a4b0449780bc7a98b6
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-10-12 10:01:29 +0200

    Add windowWidth listener

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { useState } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const MainLayout = ({ children }) => {
+  const [windowWidth, setwindowsWidth] = useState(window.innerWidth);
+  const windowListener = () => {
+    setwindowsWidth(window.innerWidth);
+    console.log(windowWidth);
+  };
+  window.addEventListener('resize', windowListener);
+
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
