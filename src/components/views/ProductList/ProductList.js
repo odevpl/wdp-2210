8213dfe3be054480bee18354585d39c9de82f4d6
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-10-16 21:12:04 +0200

    Add structure

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -2,8 +2,23 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => {
+  // ProductList.propTypes = {};
 
-// ProductList.propTypes = {};
+  return (
+    <div className={styles.root}>
+      <div className={'container ' + styles.x}>
+        <div className={'col-12 ' + styles.banner}>
+          <div>Banner</div>
+          <div>Home</div>
+        </div>
+        <div className={'col-12 ' + styles.product_container}>
+          <div className={'col-9 ' + styles.product_list}>Product List</div>
+          <div className={'col-3 ' + styles.filters}>Filters</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductList;
