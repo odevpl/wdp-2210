f2e8ac09cb988f901466bc4d082b38bec3e7778d
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-10-19 19:09:34 +0200

    Fix blog display

--- src/components/features/Blog/Blog.js
+++ src/components/features/Blog/Blog.js
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './Blog.module.scss';
 import Swipeable from '../../common/Swipeable/Swipeable';
 import clsx from 'clsx';
-const Blog =() =>{
+import { faCalendar, faComment } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-  
-
+const Blog = () => {
   return (
     <div>
       <Swipeable>
@@ -14,12 +14,11 @@ const Blog =() =>{
             <div className={styles.panelBar}>
               <div className='row no-gutters align-items-end'>
                 <div className={'col-12 col-sm-12 col-md-3 ' + styles.heading}>
-                  <h3>New furniture</h3>
+                  <h3>LATEST BLOG</h3>
                 </div>
                 <div className={'col ' + styles.menu}>
                   <ul>
-                    <li>
-                    </li>  
+                    <li></li>
                   </ul>
                 </div>
                 <div className={'col-auto ' + styles.dots}>
@@ -31,48 +30,83 @@ const Blog =() =>{
               <div className={styles.blogBox}>
                 <div className={styles.blogImg}>
                   <div className={styles.blogInfo}>
-                    <p>15 Jan 2016 4Coments</p>
+                    <p className={styles.dispaly}>
+                      <span>
+                        <FontAwesomeIcon className={styles.icon} icon={faCalendar} />
+                        15 Jan 2016
+                      </span>
+                      <span>
+                        <FontAwesomeIcon className={styles.icon} icon={faComment} />
+                        4Coments
+                      </span>
+                    </p>
                   </div>
                 </div>
-                <div className={clsx(styles.blogText, styles.textGold)}>
+                <div className={clsx(styles.blogText)}>
                   <h4>Products That Fight Static</h4>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                     when an unknown printer took a galley of type and scrambled it to make a type 
-                     specimen book. It has survived not only five centuries, but also the leap into </p>
-                  <button className={styles.goldenButton}>Read More</button>
-                </div>
-              </div>
-              <div className={clsx(styles.blogBox)}>
-                <div className={styles.blogImg}>
-                  <div className={styles.blogInfo}>
-                    <p>15 Jan 2016 4Coments</p>
-                  </div>
-                </div>
-                <div className={styles.blogText}>
-                  <h4>Products That Fight Static</h4>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                     when an unknown printer took a galley of type and scrambled it to make a type 
-                     specimen book. It has survived not only five centuries, but also the leap into </p>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industrys standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not
+                    only five centuries, but also the leap into{' '}
+                  </p>
                   <button>Read More</button>
                 </div>
               </div>
               <div className={clsx(styles.blogBox)}>
                 <div className={styles.blogImg}>
                   <div className={styles.blogInfo}>
-                    <p>15 Jan 2016 4Coments</p>
+                    <p className={styles.dispaly}>
+                      <span>
+                        <FontAwesomeIcon className={styles.icon} icon={faCalendar} />
+                        15 Jan 2016
+                      </span>
+                      <span>
+                        <FontAwesomeIcon className={styles.icon} icon={faComment} />
+                        4Coments
+                      </span>
+                    </p>
                   </div>
                 </div>
                 <div className={styles.blogText}>
                   <h4>Products That Fight Static</h4>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                     when an unknown printer took a galley of type and scrambled it to make a type 
-                     specimen book. It has survived not only five centuries, but also the leap into </p>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industrys standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not
+                    only five centuries, but also the leap into{' '}
+                  </p>
                   <button>Read More</button>
                 </div>
-                
+              </div>
+              <div className={clsx(styles.blogBox)}>
+                <div className={styles.blogImg}>
+                  <div className={styles.blogInfo}>
+                    <p className={styles.dispaly}>
+                      <span>
+                        <FontAwesomeIcon className={styles.icon} icon={faCalendar} />
+                        15 Jan 2016
+                      </span>
+                      <span>
+                        <FontAwesomeIcon className={styles.icon} icon={faComment} />
+                        4Coments
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.blogText}>
+                  <h4>Products That Fight Static</h4>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industrys standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not
+                    only five centuries, but also the leap into{' '}
+                  </p>
+                  <button>Read More</button>
+                </div>
               </div>
             </div>
           </div>
