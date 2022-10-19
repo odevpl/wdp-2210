049dd768873c398b1e728eaf131fcdf8bcbed519
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-10-19 18:45:30 +0200

    Delete conflict

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -92,14 +92,16 @@ class NewFurniture extends React.Component {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className={'row ' + this.change()}>
-            {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-              <div key={item.id} className='col-6 col-md-4 col-lg-3 '>
-                <ProductBox {...item} item={item} />
+              <div className={'row ' + this.change()}>
+                {categoryProducts
+                  .slice(activePage * 8, (activePage + 1) * 8)
+                  .map(item => (
+                    <div key={item.id} className='col-6 col-md-4 col-lg-3 '>
+                      <ProductBox {...item} item={item} />
+                    </div>
+                  ))}
               </div>
-            ))}
+            </div>
           </div>
         </Swipeable>
       </div>
